0e1b18aed8e78294365ab0840d84063879ffbd17
Ali Talebi <sudofrost@gmail.com>
2023-12-18 18:23:08 +0330

    create CRUD api for booking

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register('tabels', views.BookingViewSet, basename='tabels')
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('menu/', views.MenuView.as_view()),
     path('menu/<int:pk>', views.SingleMenuView.as_view()),
+    path('booking/', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Menu
-from .serializers import MenuSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
@@ -14,4 +15,9 @@ class MenuView(ListCreateAPIView):
 class SingleMenuView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    lookup_field = 'pk'
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
     lookup_field = 'pk'
