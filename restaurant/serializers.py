a172bcbdbed700214ceba7d0379320687584f9be
Ali Talebi <sudofrost@gmail.com>
2023-12-18 18:09:15 +0330

    create booking serializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Menu
+from .models import Menu, Booking
 
 class MenuSerializer(ModelSerializer):
     class Meta:
@@ -7,3 +7,8 @@ class MenuSerializer(ModelSerializer):
         fields = '__all__'
         read_only_fields = ('id',)
 
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+        read_only_fields = ('id',)
