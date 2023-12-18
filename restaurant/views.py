c774532a6a91c93ec2b09a9493d831abf71d53e9
Ali Talebi <sudofrost@gmail.com>
2023-12-18 18:05:35 +0330

    create CRUD api for menu

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    lookup_field = 'pk'
